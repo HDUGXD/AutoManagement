7a39fc0bba1b6e9b8032dba72d546efbfb13722d
HDUGXD <13588030464@163.com>
2019-04-12 10:41:02 +0800

    RocketMQ 启动问题解决

--- src/main/java/com/kdmt/gxd/easy/util/rocketMq/RocketMQProducer.java
+++ src/main/java/com/kdmt/gxd/easy/util/rocketMq/RocketMQProducer.java
@@ -14,13 +14,13 @@ public class RocketMQProducer {
     private DefaultMQProducer defaultMQProducer;
     private String producerGroup;
     private String namesrvAddr;
-    private String instanceName;
-    private int retryTimes;
+//    private String instanceName;
+//    private int retryTimes;
 
     public void init() throws MQClientException {
         this.defaultMQProducer = new DefaultMQProducer(this.producerGroup);
         defaultMQProducer.setNamesrvAddr(this.namesrvAddr);
-        defaultMQProducer.setInstanceName(this.instanceName);
+//        defaultMQProducer.setInstanceName(this.instanceName);
         //defaultMQProducer.setRetryTimesWhenSendFailed(this.retryTimes);
         defaultMQProducer.start();
         // logger.info("rocketMQ初始化生产者完成[producerGroup：" + producerGroup +
@@ -45,12 +45,12 @@ public class RocketMQProducer {
         this.namesrvAddr = namesrvAddr;
     }
 
-    public void setInstanceName(String instanceName) {
-        this.instanceName = instanceName;
-    }
+//    public void setInstanceName(String instanceName) {
+//        this.instanceName = instanceName;
+//    }
 
-    public void setRetryTimes(int retryTimes) {
-        this.retryTimes = retryTimes;
-    }
+//    public void setRetryTimes(int retryTimes) {
+//        this.retryTimes = retryTimes;
+//    }
 
 }
